f250d1714f2a3cfff014c029d6c4f4aac37052b9
David Olivari <david.olivari@gmail.com>
2015-06-02 19:53:11 +0200

    upload and encoding working

--- dubpan.js
+++ dubpan.js
@@ -1,13 +1,37 @@
 var express = require('express');
+var multer = require('multer');
+var childProcess = require('child_process');
+
 var app = express();
 
+app.use(multer({
+    dest: '/tmp/'
+}))
+app.use('/', express.static('public'));
 
 app.post('/upload', function(req, res) {
-    res.send('Hello World!');
+    var conProc = childProcess.exec('ffmpeg -y -i public/videos/' + req.body.video + '.webm -i ' + req.files.sound.path + ' -vcodec copy /tmp/test.webm', function(error, stdout, stderr) {
+
+        if (error) {
+            console.log(error.stack);
+            console.log('Error code: ' + error.code);
+            console.log('Signal received: ' + error.signal);
+        }
+    });
+    conProc.on('exit', function(code) {
+        console.log("done");
+        childProcess.exec('rm -f ' + req.files.sound.path, function(error, stdout, stderr) {
+            if (error) {
+                console.log(error.stack);
+                console.log('Error code: ' + error.code);
+                console.log('Signal received: ' + error.signal);
+            }
+        });
+        res.json({
+            success: true
+        });
+    });
 });
-
-
-app.use('/', express.static('public'));
 
 
 var server = app.listen(3000, function() {
